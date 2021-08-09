bea6d7c01eeadb5d89c3c0947e376d0452f8fb19
surendraguduri <88442867+surendraguduri@users.noreply.github.com>
2021-08-09 18:53:25 +0530

    Update figure.cpp

--- figure.cpp
+++ figure.cpp
@@ -5,7 +5,7 @@ int main()
     int i,j,k;
     int t=n;
 
-    for(i=1;i<=n-1;i++)
+    for(i=1;i<=n;i++)
     {
         for(j=t;j>0;j--)
         {
@@ -14,14 +14,21 @@ int main()
         printf("/");
         for(k=2;k<2*i;k++)
         {
-            printf(" ");
+            if(i==5)
+            {
+                printf("_");
+            }
+            else
+            {
+                printf(" ");
+            }
+            
         }
         printf("\\");
         printf("\n");
         printf("\n");
         t--;
     }
-    printf(" /________\\");
     
     return 0;
 }
